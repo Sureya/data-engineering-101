2cae736bc8c2dbf77f1ee9c33396da4154140275
Sureya Sathiamoorthi <sureyasathiamoorthi@Sureyas-MBP.broadband>
2019-04-01 17:44:28 +0100

    chapter 2 DDL scripts added

---
+++ chapter2/ddl.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA IF NOT EXISTS weather;
+
+CREATE TABLE IF NOT EXISTS daily_weather
+(
+	sunset DATE NOT NULL,
+	sunrise DATE NOT NULL,
+	humidity INTEGER,
+	max_temperature DOUBLE PRECISION NOT NULL,
+	min_temperature DOUBLE PRECISION NOT NULL,
+	status VARCHAR(35),
+	extract_date DATE NOT NULL constraint daily_weather_pk primary key
+);
+
+
