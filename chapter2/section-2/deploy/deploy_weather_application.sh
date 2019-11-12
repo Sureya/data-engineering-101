f8089a42ab32599fe8a3755faf61dcaf1e16a495
Sureya Sathiamoorthi <sureya.sathiamoorthi@cloudreach.com>
2019-11-12 15:28:03 +0000

    Chapter-2 Ansible script tested

--- chapter2/section-2/deploy/deploy_weather_application.sh
+++ chapter2/section-2/deploy/deploy_weather_application.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+set -e #Fail on first error
+
+#***********************************************************
 # All these environment values must be set before we can execute the script.
 export ENV="dev"
-export API_KEY="432725751d6400d23f3652784b3d5938"
-export DB_NAME="data_enginner"
+export API_KEY="<YOUR-API-KEY>"
+export DB_NAME="postgres"
 export DB_USER="developer"
 export DB_PWD="developer"
-export DB_HOST="localhost"
-export EC2_DNS_NAME="ec2-3-8-17-211.eu-west-2.compute.amazonaws.com"
-export AWS_AUTH_FILE="~/.ssh/ec2_keypair_london.pem"
+export DB_HOST="<YOUR-HOST-NAME-FROM RDS>"
+export EC2_DNS_NAME="<YOUR-EC2-INSTANCE-IPV4-PUBLIC-DNS>"
+export AWS_AUTH_FILE="<FULL/PATH/TO/YOUR/EXECUTE.pem>"
+#***********************************************************
 
 # This is your SSH key used for Github authentication
-ssh-add -K ~/.ssh/cloudReach                                                                                                                                                                                                                                                                                                                                99 ↵
+ssh-add -K ~/.ssh/id_rsa # If you have used different KEY file for authenticating GitHub please change the filename
 
-echo  '[aws]' > ${ENV}
+echo  '[aws]' >> ${ENV}
 echo ${EC2_DNS_NAME} >> ${ENV}
 # The above two lines creates hosts file and add your EC2 instance to your host file.
-
 ansible-playbook weather.yml --private-key=${AWS_AUTH_FILE} -K -u ubuntu -i ${ENV}
